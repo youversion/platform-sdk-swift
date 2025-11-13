4582cb8ba7b25e775008329da2b1cb5a5f6c0363
Jeff Hampton <jhampton@gmail.com>
2025-11-13 16:47:15 -0500

    chore(lint): bump body-max-line-length to 300 characters, min 2 characters

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,22 +1,22 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "body-max-line-length": [2, "always", 200],
+    "body-max-line-length": [2, "always", 300],
     "type-enum": [
       2,
       "always",
       [
-        "feat", // New feature
-        "fix", // Bug fix
-        "docs", // Documentation only
-        "style", // Code style changes (formatting, etc.)
-        "refactor", // Code refactoring
-        "perf", // Performance improvements
-        "test", // Adding/updating tests
-        "build", // Build system changes
-        "ci", // CI/CD changes
-        "chore", // Maintenance tasks
-        "revert", // Revert previous commit
+        "feat", // New feature - triggers minor version increment
+        "fix", // Bug fix - triggers patch version increment
+        "docs", // Documentation only - no version increment
+        "style", // Code style changes (formatting, etc.) - no version increment
+        "refactor", // Code refactoring - no version increment
+        "perf", // Performance improvements - no version increment
+        "test", // Adding/updating tests - no version increment
+        "build", // Build system changes - no version increment
+        "ci", // CI/CD changes - no version increment
+        "chore", // Maintenance tasks - no version increment
+        "revert", // Revert previous commit - no version increment
       ],
     ],
   },
