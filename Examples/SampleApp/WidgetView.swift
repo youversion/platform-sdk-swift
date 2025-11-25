42c1cf5788325db6584becc191f6a0a47629872c
David Fedor <david.fedor@youversion.com>
2025-11-25 13:03:07 -0600

    simplify the WidgetView

--- Examples/SampleApp/WidgetView.swift
+++ Examples/SampleApp/WidgetView.swift
@@ -3,16 +3,12 @@ import YouVersionPlatform
 
 struct WidgetView: View {
     var body: some View {
-        VStack {
-            Spacer()
-            BibleWidgetView(
-                reference: BibleReference(
-                    versionId: 111, bookUSFM: "2CO", chapter: 1, verseStart: 3, verseEnd: 4
-                ),
-                fontSize: 18
-            )
-            Spacer()
-        }
+        BibleWidgetView(
+            reference: BibleReference(
+                versionId: 111, bookUSFM: "2CO", chapter: 1, verseStart: 3, verseEnd: 4
+            ),
+            fontSize: 18
+        )
     }
 }
 
