23f40f6ba1ceb45f7b9640accf66e28ccd30e0e7
David Fedor <david.fedor@youversion.com>
2026-02-24 14:12:42 -0600

    remove test scrollview

--- Examples/SampleApp/WidgetView.swift
+++ Examples/SampleApp/WidgetView.swift
@@ -3,14 +3,12 @@ import YouVersionPlatform
 
 struct WidgetView: View {
     var body: some View {
-        ScrollView {
-            BibleWidgetView(
-                reference: BibleReference(
-                    versionId: 3034, bookUSFM: "2CO", chapter: 1, verseStart: 3, verseEnd: 4
-                ),
-                fontSize: 18
-            )
-        }
+        BibleWidgetView(
+            reference: BibleReference(
+                versionId: 3034, bookUSFM: "2CO", chapter: 1, verseStart: 3, verseEnd: 4
+            ),
+            fontSize: 18
+        )
     }
 }
 
