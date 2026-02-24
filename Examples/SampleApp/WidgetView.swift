540e945b292ff8387dafa124b06d6fd5be469a9f
David Fedor <david.fedor@youversion.com>
2026-02-24 14:11:57 -0600

    undo the test additions

--- Examples/SampleApp/WidgetView.swift
+++ Examples/SampleApp/WidgetView.swift
@@ -6,31 +6,7 @@ struct WidgetView: View {
         ScrollView {
             BibleWidgetView(
                 reference: BibleReference(
-                    versionId: 111, bookUSFM: "GEN", chapter: 2, verseStart: 1, verseEnd: 3
-                ),
-                fontSize: 18
-            )
-            BibleWidgetView(
-                reference: BibleReference(
-                    versionId: 111, bookUSFM: "GEN", chapter: 2, verseStart: 1, verseEnd: 4
-                ),
-                fontSize: 18
-            )
-            BibleWidgetView(
-                reference: BibleReference(
-                    versionId: 111, bookUSFM: "GEN", chapter: 2, verseStart: 3, verseEnd: 4
-                ),
-                fontSize: 18
-            )
-            BibleWidgetView(
-                reference: BibleReference(
-                    versionId: 111, bookUSFM: "MAT", chapter: 5, verseStart: 1, verseEnd: 1
-                ),
-                fontSize: 18
-            )
-            BibleWidgetView(
-                reference: BibleReference(
-                    versionId: 111, bookUSFM: "MAT", chapter: 5, verseStart: 1, verseEnd: 2
+                    versionId: 3034, bookUSFM: "2CO", chapter: 1, verseStart: 3, verseEnd: 4
                 ),
                 fontSize: 18
             )
