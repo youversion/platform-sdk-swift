659dd9204b321d2b078c88da5ee973e20af265df
Anthony Raj <anthony.raj@youversion.com>
2026-04-17 16:33:07 -0500

    Adding a dotted pattern

--- Sources/YouVersionPlatformUI/Views/VerseSelectionStyle.swift
+++ Sources/YouVersionPlatformUI/Views/VerseSelectionStyle.swift
@@ -12,6 +12,8 @@ public struct VerseSelectionStyle: Sendable {
     }
     /// Solid gray underline (SDK default).
     public static let solid = VerseSelectionStyle()
-    /// Dashed gray underline.
-    public static let dashed = VerseSelectionStyle(strokeStyle: StrokeStyle(lineWidth: 0.5, dash: [4, 2]))
+    /// Dashed primar underline.
+    public static let dashed = VerseSelectionStyle(color: .primary, strokeStyle: StrokeStyle(lineWidth: 0.5, dash: [4, 2]))
+    /// Dotted underline
+    public static let dotted = VerseSelectionStyle(color: .primary, strokeStyle: StrokeStyle(lineWidth: 1.5, lineCap: .round, dash: [0.5, 4]))
 }
